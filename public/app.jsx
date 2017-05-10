0ff83aee3491f57534fa7e9b319eb84bcc2c7634
john_lee <jjlee099@gmail.com>
2017-05-10 14:41:07 -0700

    adds first component

--- public/app.jsx
+++ public/app.jsx
@@ -1,4 +1,27 @@
+var Greeter = React.createClass({
+  render: function(){
+    return(
+      <div>
+        <h1>Hello React!</h1>
+      <p>Test</p>
+      </div>
+    )
+  }
+});
+
+var Greeter2 = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p>Test #2?</p>
+      </div>
+    )
+  }
+})
+
+
 ReactDOM.render(
-  <h1>Hello React!</h1>,
+  <Greeter/>,
+// <Greeter2/>,
   document.getElementById('app')
 );
