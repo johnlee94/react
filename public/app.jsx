6deabae39cc1188406071182769192db0d745164
john_lee <jjlee099@gmail.com>
2017-05-10 14:51:50 -0700

    tests babel

--- public/app.jsx
+++ public/app.jsx
@@ -1,27 +1,20 @@
 var Greeter = React.createClass({
   render: function(){
-    return(
-      <div>
-        <h1>Hello React!</h1>
-      <p>Test</p>
-      </div>
+    return React.createElement(
+      'h1',
+      null,
+      'Hello React!'
     )
+    // return(
+    //   <div>
+    //     <h1>Hello React!</h1>
+    //   <p>Test</p>
+    //   </div>
+    // )
   }
 });
 
-var Greeter2 = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <p>Test #2?</p>
-      </div>
-    )
-  }
-})
-
-
 ReactDOM.render(
   <Greeter/>,
-// <Greeter2/>,
   document.getElementById('app')
 );
